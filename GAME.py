537fd6b90d3f2520c6d72dc2f542c975437f9a7a
ozioo <62998144+ozioo@users.noreply.github.com>
2021-04-18 13:48:27 +0100

    Update GAME.py

--- GAME.py
+++ GAME.py
@@ -2,7 +2,15 @@ import sys
 import pygame
 import math
 from pygame.locals import *
- 
+
+
+UNLOCK = pygame.image.load('pixilart-drawing.png')
+
+LOCK = pygame.image.load('pixil-frame-0.png')
+
+CURSOR = pygame.image.load('pixil-frame-0 (1).png')
+
+INVIS = pygame.image.load('pixil-frame-0 (2).png')
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -15,6 +23,31 @@ pygame.display.set_mode((1000, 1000), 0, 32)
 screen = pygame.display.get_surface()
 pygame.init()
 
+#class Cursor(pygame.sprite.Sprite):
+#   def __init__(self,  x, y):
+#
+#    def update(self):
+#
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, image, x_pos, y_pos):
+        super().__init__()
+        # Set height, width
+        self.image = pygame.Surface([40, 40])
+        self.image= image
+        self.mask = pygame.mask.from_surface(self.image)
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.y = y_pos
+        self.rect.x = x_pos
+    def change_state(self, image):
+        self.image = image
+        self.mask = pygame.mask.from_surface(self.image)
+    #def update(self):
+
+
+    
+
+
 class Line(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
@@ -23,7 +56,7 @@ class Line(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = (400, 400))
         self.angle = 0
     def update(self):
-        self.angle +=2
+        self.angle +=4
         if self.angle >=360:
             deg=0
         for i in range(1, 10):
@@ -31,12 +64,13 @@ class Line(pygame.sprite.Sprite):
             dy = y/2 + x/2 * math.sin(math.radians(self.angle-.1*i))
             f = i*.1
             pygame.draw.aaline(screen, (0, int(255/(1+f)), 0), (int(x/2), int(y/2)), (dx, dy),5)
-
         self.mask = pygame.mask.from_surface(self.image)
  
 # Set the width and height of the screen [width, height]
 
- 
+#def radar_detection
+
+#def lock
 pygame.display.set_caption("Missile Command Redux")
 # Loop until the user clicks the close button.
 done = False
@@ -46,7 +80,9 @@ clock = pygame.time.Clock()
 
 all_sprites_list = pygame.sprite.Group()
 radar =Line(400,400)
+enemy = Enemy(UNLOCK,200,200)
 all_sprites_list.add(radar)
+all_sprites_list.add(enemy)
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -74,7 +110,7 @@ while not done:
 
     all_sprites_list.update()
     pygame.display.update()     
-    pygame.time.wait(30)        
+    clock.tick(30)        
     screen.fill((0, 0, 0, 0))    
     # --- Screen-clearing code goes here
  
